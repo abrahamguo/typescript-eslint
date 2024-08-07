2a41aff463fbdf3af428b249cc1e5c1cc94b03ec
Abraham Guo <aguo@precisionplanting.com>
2024-08-06 20:22:35 -0500

    enable rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -319,6 +319,7 @@ export default tseslint.config(
       //
 
       'jsdoc/informative-docs': 'error',
+      'unicorn/custom-error-definition': 'error',
       'unicorn/no-typeof-undefined': 'error',
       'unicorn/no-useless-spread': 'error',
     },
